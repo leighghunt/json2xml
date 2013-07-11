e9b0a9e560c0bd8139dd9c40397ea516c760b74c
Leigh Hunt <leigh.hunt@splicegroup.co.nz>
2013-07-11 16:27:54 +1200

    First working version

--- json2xml/Form1.Designer.cs
+++ json2xml/Form1.Designer.cs
@@ -28,12 +28,91 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtJSON = new System.Windows.Forms.TextBox();
+            this.txtEmbeddedJSON = new System.Windows.Forms.TextBox();
+            this.lblErrorMessage = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "&JSON:";
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 250);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(92, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "&Embedded JSON:";
+            // 
+            // txtJSON
+            // 
+            this.txtJSON.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtJSON.Location = new System.Drawing.Point(111, 13);
+            this.txtJSON.Multiline = true;
+            this.txtJSON.Name = "txtJSON";
+            this.txtJSON.Size = new System.Drawing.Size(357, 250);
+            this.txtJSON.TabIndex = 2;
+            this.txtJSON.TextChanged += new System.EventHandler(this.txtJSON_TextChanged);
+            // 
+            // txtEmbeddedJSON
+            // 
+            this.txtEmbeddedJSON.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtEmbeddedJSON.Location = new System.Drawing.Point(111, 243);
+            this.txtEmbeddedJSON.Name = "txtEmbeddedJSON";
+            this.txtEmbeddedJSON.Size = new System.Drawing.Size(357, 20);
+            this.txtEmbeddedJSON.TabIndex = 3;
+            this.txtEmbeddedJSON.TextChanged += new System.EventHandler(this.txtEmbeddedJSON_TextChanged);
+            // 
+            // lblErrorMessage
+            // 
+            this.lblErrorMessage.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.lblErrorMessage.AutoSize = true;
+            this.lblErrorMessage.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblErrorMessage.ForeColor = System.Drawing.Color.Red;
+            this.lblErrorMessage.Location = new System.Drawing.Point(16, 267);
+            this.lblErrorMessage.Name = "lblErrorMessage";
+            this.lblErrorMessage.Size = new System.Drawing.Size(97, 13);
+            this.lblErrorMessage.TabIndex = 4;
+            this.lblErrorMessage.Text = "lblErrorMessage";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(480, 380);
+            this.Controls.Add(this.lblErrorMessage);
+            this.Controls.Add(this.txtEmbeddedJSON);
+            this.Controls.Add(this.txtJSON);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "json2xml";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtJSON;
+        private System.Windows.Forms.TextBox txtEmbeddedJSON;
+        private System.Windows.Forms.Label lblErrorMessage;
     }
 }
 
--- json2xml/Form1.cs
+++ json2xml/Form1.cs
@@ -12,9 +12,61 @@ namespace json2xml
 {
     public partial class Form1 : Form
     {
+        private bool _updatingControls = false;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void txtJSON_TextChanged(object sender, EventArgs e)
+        {
+            updateJSON(sender);
+        }
+
+        private void txtEmbeddedJSON_TextChanged(object sender, EventArgs e)
+        {
+            updateJSON(sender);
+        }
+
+        private void updateJSON(object sender)
+        {
+            try
+            {
+                if (_updatingControls)
+                {
+                    return;
+                }
+
+                _updatingControls = true;
+                if (sender is TextBox)
+                {
+                    var jsonObject = Newtonsoft.Json.JsonConvert.DeserializeObject(((TextBox)sender).Text);
+
+                    if (sender == txtJSON)
+                    {
+                        txtEmbeddedJSON.Text = Newtonsoft.Json.JsonConvert.SerializeObject(jsonObject, Newtonsoft.Json.Formatting.None).Replace("\"", "'");
+                    }
+
+                    if (sender == txtEmbeddedJSON)
+                    {
+                        txtJSON.Text = Newtonsoft.Json.JsonConvert.SerializeObject(jsonObject, Newtonsoft.Json.Formatting.Indented);
+                    }
+                }
+                lblErrorMessage.Text = "";
+
+                _updatingControls = false;
+            }
+            catch (Exception ex)
+            {
+                lblErrorMessage.Text = ex.Message;
+                _updatingControls = false;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            lblErrorMessage.Text = "";
         }
     }
 }
